ce27b082daacb46caad7df4b18d79e63dc58657d
beppe11 <78246566+beppe11@users.noreply.github.com>
2022-10-23 12:35:46 +0900

    FirstReactProject

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
 
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <h1 style={{ color: "orange" }}>こんにちは！</h1>
       <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
